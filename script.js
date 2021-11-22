3ecdb16e8d45a66aad91cb2c066e51981cce0007
Pavel Krasnov <billiardist94@gmail.com>
2021-11-22 13:44:00 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper("reviews__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
