f7b1dad14c3b8407697788aa7a1bcd476bdcc8e4
Pavel Krasnov <billiardist94@gmail.com>
2021-11-29 16:32:16 +0300

    initial commit

--- script.js
+++ script.js
@@ -78,3 +78,41 @@ const swiper3 = new Swiper(".swiper3", {
         },
     },
 });
+
+// ___Menu-Burger___
+
+const openBurgerBtn = document.querySelector('#open-burger-btn');
+const closeBurgerBtn = document.querySelector('#close-burger-btn');
+const menuBurger = document.querySelector('.menu__burger');
+const content = document.querySelector('.content');
+const links = document.querySelectorAll('.burger__link');
+
+function openBurger(e) {
+    menuBurger.classList.add('menu__burger-opened');
+}
+
+function closeBurger(e) {
+    menuBurger.classList.remove('menu__burger-opened');
+}
+
+openBurgerBtn.addEventListener('click', openBurger);
+closeBurgerBtn.addEventListener('click', closeBurger);
+content.addEventListener('click', closeBurger);
+window.addEventListener('scroll', (e) => {
+    if(window.innerHeight > 590) {
+        closeBurger()
+    }
+});
+
+links.forEach((el) => {
+    el.addEventListener('click', closeBurger);
+})
+
+// ___Paralax-effect___
+
+window.onload = () => {
+    document.querySelector('.content').style.backgroundAttachment = "fixed";
+    window.onscroll = function () {
+    document.querySelector('.content').style.backgroundPosition = "0px " + (0 + (Math.max(document.documentElement.scrollTop, document.body.scrollTop) / 8)) + "px";
+    }
+};
